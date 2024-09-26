7cab30e3ea53a88456e635bdb120feae89b2dd7b
raurau4 <elenzita02@gmail.com>
2024-09-26 17:54:35 -0300

    ryutnhgfdn

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.comprar-btn');
-    
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Produto adicionado ao carrinho!');
-        });
-    });
-});
